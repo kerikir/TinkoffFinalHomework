2e42e6b40cde54aed8f7b4fefac1f2657dbbb908
kerikir <vlasov-kerik@mail.ru>
2025-12-18 22:09:27 +0300

    upgraded the libraries and compileSdk versions in the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     namespace = "com.tinkoff.android_homework"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.tinkoff.android_homework"
