be09066dd47cf00d37027bc8219489eabd6d366e
kerikir <vlasov-kerik@mail.ru>
2025-12-19 14:59:03 +0300

    added the OperationType model in the domain layer

---
+++ app/src/main/java/com/tinkoff/android_homework/domain/main/models/OperationType.kt
@@ -0,0 +1,12 @@
+package com.tinkoff.android_homework.domain.main.models
+
+/**
+ * Тип финансовой операции.
+ * Модель из domain слоя.
+ */
+enum class OperationType {
+    /** Оплата товаров и услуг */
+    OUTCOME,
+    /** Входящий перевод */
+    INCOME,
+}
