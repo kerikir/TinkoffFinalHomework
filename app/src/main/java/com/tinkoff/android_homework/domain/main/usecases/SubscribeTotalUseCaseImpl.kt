5136b139c3dad79dc660833f7e06bd43050cfd38
kerikir <vlasov-kerik@mail.ru>
2025-12-19 17:19:15 +0300

    deleted the dependency on the data layer in the domain layer

--- app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeTotalUseCaseImpl.kt
+++ app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeTotalUseCaseImpl.kt
@@ -1,9 +1,10 @@
 package com.tinkoff.android_homework.domain.main.usecases
 
-import com.tinkoff.android_homework.data.network.repo.total.TotalRepository
 import com.tinkoff.android_homework.domain.main.entities.Total
+import com.tinkoff.android_homework.domain.main.repos.TotalRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
+
 
 class SubscribeTotalUseCaseImpl @Inject constructor(
     private val repository: TotalRepository,
