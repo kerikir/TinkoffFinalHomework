070c3f68a5465f44a42d1d3c24f2634053b75061
kerikir <vlasov-kerik@mail.ru>
2025-12-19 14:50:39 +0300

    added the comments in the Detail.kt

--- app/src/main/java/com/tinkoff/android_homework/domain/main/entities/Detail.kt
+++ app/src/main/java/com/tinkoff/android_homework/domain/main/entities/Detail.kt
@@ -4,13 +4,13 @@ package com.tinkoff.android_homework.domain.main.entities
 import com.tinkoff.android_homework.presentation.model.operations.OperationType
 
 /**
- * Детальное описание денежной операции.
+ * Детальное описание финансовой операции.
  * Модель данных для domain слоя.
  */
 data class Detail(
-    /** Тип денежной операции */
+    /** Тип финансовой операции */
     val type: OperationType,
-    /** Комментарий к денежной операции для отображения */
+    /** Комментарий к финансовой операции для отображения */
     val comment: String,
     /** Список позиций (что входит в сумму операции) */
     val positions: List<String>,
