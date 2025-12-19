5e05c305221f0443ef4347456b6ca8fd012143dc
kerikir <vlasov-kerik@mail.ru>
2025-12-19 17:17:21 +0300

    added the use case implemented for the total in the domain layer

---
+++ app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeTotalUseCaseImpl.kt
@@ -0,0 +1,15 @@
+package com.tinkoff.android_homework.domain.main.usecases
+
+import com.tinkoff.android_homework.data.network.repo.total.TotalRepository
+import com.tinkoff.android_homework.domain.main.entities.Total
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SubscribeTotalUseCaseImpl @Inject constructor(
+    private val repository: TotalRepository,
+) : SubscribeTotalUseCase {
+
+    override suspend fun invoke(): Flow<Total> {
+        return repository.subscribeTotal()
+    }
+}
