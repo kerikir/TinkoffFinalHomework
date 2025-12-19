b31f3a83760e54dc89bcca7109a510f346005287
kerikir <vlasov-kerik@mail.ru>
2025-12-19 14:47:46 +0300

    added the comments in the Detail.kt

--- app/src/main/java/com/tinkoff/android_homework/domain/main/entities/Detail.kt
+++ app/src/main/java/com/tinkoff/android_homework/domain/main/entities/Detail.kt
@@ -1,12 +1,17 @@
 package com.tinkoff.android_homework.domain.main.entities
 
+// TODO убрать тип операции из слоя presentation
 import com.tinkoff.android_homework.presentation.model.operations.OperationType
 
 /**
- * @author d.shtaynmets
+ * Детальное описание денежной операции.
+ * Модель данных для domain слоя.
  */
 data class Detail(
+    /** Тип денежной операции */
     val type: OperationType,
+    /** Комментарий к денежной операции для отображения */
     val comment: String,
+    /** Список позиций (что входит в сумму операции) */
     val positions: List<String>,
 )
