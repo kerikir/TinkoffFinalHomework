49e139368ba050e49a3f57f451fba1aa88442114
kerikir <vlasov-kerik@mail.ru>
2025-12-19 17:34:56 +0300

    added the use case implemented for the detail in the domain layer

---
+++ app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeDetailUseCaseImpl.kt
@@ -0,0 +1,14 @@
+package com.tinkoff.android_homework.domain.main.usecases
+
+import com.tinkoff.android_homework.data.network.repo.detail.DetailRepository
+import com.tinkoff.android_homework.domain.main.entities.Detail
+import javax.inject.Inject
+
+class SubscribeDetailUseCaseImpl @Inject constructor(
+    private val detailRepository: DetailRepository
+) : SubscribeDetailUseCase {
+
+    override suspend fun getDetail(id: Int): Detail {
+        return detailRepository.getDetail(id)
+    }
+}
