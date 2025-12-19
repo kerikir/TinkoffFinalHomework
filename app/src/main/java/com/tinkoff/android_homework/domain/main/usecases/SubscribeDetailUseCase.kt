615c4813376e2fdd1c2b1bca757f113ddd5b92fd
kerikir <vlasov-kerik@mail.ru>
2025-12-19 17:42:16 +0300

    added the comments in the SuscribeDetailUseCase.kt and SuscribeDetailUseCaseImpl.kt

--- app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeDetailUseCase.kt
+++ app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeDetailUseCase.kt
@@ -3,6 +3,11 @@ package com.tinkoff.android_homework.domain.main.usecases
 import com.tinkoff.android_homework.domain.main.entities.Detail
 
 
+/**
+ * Описание Use Case для получения информации о финансовой операции.
+ *
+ * Интерфейс Use Case в domain слое.
+ */
 interface SubscribeDetailUseCase {
 
     suspend fun getDetail(id: Int): Detail
--- app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeDetailUseCaseImpl.kt
+++ app/src/main/java/com/tinkoff/android_homework/domain/main/usecases/SubscribeDetailUseCaseImpl.kt
@@ -5,10 +5,24 @@ import com.tinkoff.android_homework.domain.main.repos.DetailRepository
 import javax.inject.Inject
 
 
+/**
+ * Use Case для получения дополнительной информации о финансовой операции.
+ *
+ * Use Case в domain слое.
+ * Реализация бизнес-логики.
+ *
+ * @param detailRepository Откуда будут приходить данные
+ */
 class SubscribeDetailUseCaseImpl @Inject constructor(
     private val detailRepository: DetailRepository
 ) : SubscribeDetailUseCase {
 
+    /**
+     * Получении дополнительной информации о финансовой операции
+     *
+     * @param id Идентификатор финансовой информации
+     * @return Информация о финансовой операции
+     */
     override suspend fun getDetail(id: Int): Detail {
         return detailRepository.getDetail(id)
     }
