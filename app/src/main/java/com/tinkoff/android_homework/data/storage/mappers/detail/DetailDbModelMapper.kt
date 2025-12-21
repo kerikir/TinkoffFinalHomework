da29309f50515ca39d4a2762cada7f515fcf3b58
kerikir <vlasov-kerik@mail.ru>
2025-12-21 18:47:36 +0300

    added the comments in the DetailDbModelMapper.kt

--- app/src/main/java/com/tinkoff/android_homework/data/storage/mappers/detail/DetailDbModelMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/mappers/detail/DetailDbModelMapper.kt
@@ -7,12 +7,22 @@ import javax.inject.Inject
 
 
 /**
- * @author d.shtaynmets
+ * Преобразователь детального описания финансовой операции из типа data-слоя (storage)
+ * в тип domain-слоя.
+ *
+ * @param operationTypeMapper Преобразователь из типа операции data-слоя (storage) в тип операций domain-слоя
  */
 class DetailDbModelMapper @Inject constructor(
     private val operationTypeMapper: StorageOperationTypeMapper
 ) : (DetailDbModel) -> Detail {
 
+    /**
+     *  Преобразователь детального описания финансовой операции из типа data-слоя (storage)
+     *  в тип domain-слоя.
+     *
+     *  @param detailDbModel Детальное описание финансовой операции data-слоя.
+     *  @return Детальное описание финансовой операции domain-слоя.
+     */
     override fun invoke(detailDbModel: DetailDbModel): Detail {
         return Detail(
             type = operationTypeMapper(detailDbModel.type),
