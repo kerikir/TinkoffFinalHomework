d7ec4ecc5eb5aa654501d98839fb5f742d71397b
kerikir <vlasov-kerik@mail.ru>
2025-12-21 17:14:28 +0300

    added the comments in the OperationsDTO.kt

--- app/src/main/java/com/tinkoff/android_homework/data/network/entities/operations/OperationsDTO.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/entities/operations/OperationsDTO.kt
@@ -2,10 +2,18 @@ package com.tinkoff.android_homework.data.network.entities.operations
 
 import kotlinx.serialization.Serializable
 
+
 /**
- * @author d.shtaynmets
+ * Список финансовых операций.
+ *
+ * Автоматическое преобразование из JSON в Kotlin и наоборот.
+ *
+ * Модель данных для data слоя (network).
+ *
+ * Поля JSON: operation.
  */
 @Serializable
 data class OperationsDTO(
+    /** Список финансовых операций */
     val operation: List<OperationDTO>,
 )
