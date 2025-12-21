3c43a08db036703399b3b6a90fb1cbcbcc1b7e0c
kerikir <vlasov-kerik@mail.ru>
2025-12-21 16:47:23 +0300

    added the comments in the OperationType.kt

--- app/src/main/java/com/tinkoff/android_homework/data/network/models/OperationType.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/models/OperationType.kt
@@ -6,15 +6,24 @@ import kotlinx.serialization.Serializable
 
 /**
  * Тип финансовой операции.
+ * Автоматическая сериализация и десериализация класса.
  *
  * Модель из data слоя (network).
  */
 @Serializable
 enum class OperationType {
-    /** Оплата товаров и услуг */
+    /**
+     * Оплата товаров и услуг.
+     *
+     * Имя в JSON - OUTCOME.
+     */
     @SerialName("OUTCOME")
     OUTCOME,
-    /** Входящий перевод */
+    /**
+     * Входящий перевод.
+     *
+     * Имя в JSON - INCOME.
+     */
     @SerialName("INCOME")
     INCOME
 }
