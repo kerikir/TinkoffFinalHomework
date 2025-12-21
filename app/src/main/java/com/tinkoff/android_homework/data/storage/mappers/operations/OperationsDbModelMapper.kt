0b52f39c7c0de489a8dcb7495ae2ee5153a3af01
kerikir <vlasov-kerik@mail.ru>
2025-12-21 18:55:30 +0300

    added the operation type mapper in the OperationsDbModelMapper constructor

--- app/src/main/java/com/tinkoff/android_homework/data/storage/mappers/operations/OperationsDbModelMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/mappers/operations/OperationsDbModelMapper.kt
@@ -1,6 +1,7 @@
 package com.tinkoff.android_homework.data.storage.mappers.operations
 
 import com.tinkoff.android_homework.data.storage.entities.OperationDbModel
+import com.tinkoff.android_homework.data.storage.mappers.models.StorageOperationTypeMapper
 import com.tinkoff.android_homework.domain.main.entities.Operation
 import com.tinkoff.android_homework.domain.main.entities.Operations
 import javax.inject.Inject
@@ -9,7 +10,9 @@ import javax.inject.Inject
 /**
  * @author d.shtaynmets
  */
-class OperationsDbModelMapper @Inject constructor() : (List<OperationDbModel>) -> Operations {
+class OperationsDbModelMapper @Inject constructor(
+    private val operationTypeMapper: StorageOperationTypeMapper
+) : (List<OperationDbModel>) -> Operations {
 
     override fun invoke(operations: List<OperationDbModel>): Operations {
         return Operations(
