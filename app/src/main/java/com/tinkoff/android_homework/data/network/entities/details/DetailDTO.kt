e86375a74dc03f4125fae746fe9d4d62ca0f16bf
kerikir <vlasov-kerik@mail.ru>
2025-12-21 16:51:36 +0300

    added the comments in the DetailDTO.kt

--- app/src/main/java/com/tinkoff/android_homework/data/network/entities/details/DetailDTO.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/entities/details/DetailDTO.kt
@@ -4,12 +4,20 @@ import com.tinkoff.android_homework.data.network.models.OperationType
 import kotlinx.serialization.Serializable
 
 /**
- * @author d.shtaynmets
+ * Детальное описание финансовой операции.
+ *
+ * Модель данных для data слоя (network).
+ *
+ * Поля JSON: type, amount, comment, positions.
  */
 @Serializable
 data class DetailDTO(
+    /** Тип финансовой операции */
     val type: OperationType,
+    /** Сумма финансовой операции */
     val amount: Int,
+    /** Комментарий к финансовой операции для отображения */
     val comment: String,
+    /** Список позиций (что входит в сумму операции) */
     val positions: List<String>
 )
