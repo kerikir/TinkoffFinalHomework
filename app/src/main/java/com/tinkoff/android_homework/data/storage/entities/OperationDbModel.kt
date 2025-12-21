7e25843c38dfe59624e37e3bfdb860a3a0957f51
kerikir <vlasov-kerik@mail.ru>
2025-12-21 14:18:19 +0300

    added the comments in the OperationDbModel.kt

--- app/src/main/java/com/tinkoff/android_homework/data/storage/entities/OperationDbModel.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/entities/OperationDbModel.kt
@@ -8,20 +8,40 @@ import com.tinkoff.android_homework.data.storage.models.OperationType
 
 
 /**
- * @author d.shtaynmets
+ * Финансовая операция.
+ *
+ * Модель данных для data слоя (storage).
+ *
+ * Имя таблицы - operation_table.
+ * Поля: id, type, name, amount.
  */
 @Entity(tableName = OPERATION_TABLE_NAME)
 data class OperationDbModel(
+    /**
+     * Идентификатор в базе данных для финансовой операции.
+     * Первичный ключ.
+     */
     @PrimaryKey()
     val id: Long,
+    /** Тип финансовой операции */
     val type: OperationType,
+    /**
+     * Название финансовой операции.
+     * Имя столбца в БД - name.
+     */
     @ColumnInfo(name = "name")
     val name: String,
+    /**
+     * Сумма финансовой операции.
+     * Имя столбца в БД - amount.
+     */
     @ColumnInfo(name = "amount")
     val amount: Int,
 ) {
 
+    /** Хранение констант */
     companion object {
+        /** Имя таблицы в базе данных */
         const val OPERATION_TABLE_NAME = "operation_table"
     }
 }
