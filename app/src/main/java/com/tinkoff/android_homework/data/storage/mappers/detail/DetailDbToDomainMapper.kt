de0087e839947bf031abcf7153f3029faa8e0c22
kerikir <vlasov-kerik@mail.ru>
2025-12-21 18:34:33 +0300

    added the operation type mapper in the DetailDbToDomainMapper constructor

--- app/src/main/java/com/tinkoff/android_homework/data/storage/mappers/detail/DetailDbToDomainMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/mappers/detail/DetailDbToDomainMapper.kt
@@ -1,17 +1,21 @@
 package com.tinkoff.android_homework.data.storage.mappers.detail
 
 import com.tinkoff.android_homework.data.storage.entities.DetailDbModel
+import com.tinkoff.android_homework.data.storage.mappers.models.StorageOperationTypeMapper
 import com.tinkoff.android_homework.domain.main.entities.Detail
 import javax.inject.Inject
+
 
 /**
  * @author d.shtaynmets
  */
-class DetailDbToDomainMapper @Inject constructor() : (DetailDbModel) -> Detail {
+class DetailDbToDomainMapper @Inject constructor(
+    private val operationTypeMapper: StorageOperationTypeMapper
+) : (DetailDbModel) -> Detail {
 
     override fun invoke(detailDbModel: DetailDbModel): Detail {
         return Detail(
-            type = detailDbModel.type,
+            type = operationTypeMapper(detailDbModel.type),
             comment = detailDbModel.comment,
             positions = detailDbModel.positions
         )
