06561ab66c84cd4841cba5a11bc0e0eb0646ef14
kerikir <vlasov-kerik@mail.ru>
2025-12-22 15:51:56 +0300

    added the comments in the OperationsDtoMapper.kt

--- app/src/main/java/com/tinkoff/android_homework/data/network/mappers/operations/OperationsDtoMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/mappers/operations/OperationsDtoMapper.kt
@@ -5,13 +5,25 @@ import com.tinkoff.android_homework.data.network.mappers.models.NetworkOperation
 import com.tinkoff.android_homework.data.storage.entities.OperationDbModel
 import javax.inject.Inject
 
+
 /**
- * @author d.shtaynmets
+ * Преобразователь списка финансовых операций из типа data-слоя (network)
+ * в тип data-слоя (storage).
+ *
+ * @param operationTypeMapper Преобразователь из типа операции data-слоя (network)
+ * в тип операций data-слоя (storage)
  */
 class OperationsDtoMapper @Inject constructor(
     private val operationTypeMapper: NetworkOperationTypeMapper
 ) : (OperationsDTO) -> List<OperationDbModel> {
 
+    /**
+     *  Преобразователь списка финансовых операций из типа data-слоя (network)
+     *  в тип data-слоя (storage).
+     *
+     *  @param operationsDTO Список финансовых операций data-слоя (network).
+     *  @return Список финансовых операций data-слоя (storage).
+     */
     override fun invoke(operationsDTO: OperationsDTO): List<OperationDbModel> {
         return operationsDTO.operation.mapIndexed { index, operation ->
             OperationDbModel(
