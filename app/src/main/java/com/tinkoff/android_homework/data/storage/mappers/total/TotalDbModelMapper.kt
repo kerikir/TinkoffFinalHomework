ba4f503e7e9933dd3ba72d83fd9a70bcbd5f67f7
kerikir <vlasov-kerik@mail.ru>
2025-12-22 15:01:17 +0300

    added the comments in the TotalDbModelMapper.kt

--- app/src/main/java/com/tinkoff/android_homework/data/storage/mappers/total/TotalDbModelMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/mappers/total/TotalDbModelMapper.kt
@@ -5,9 +5,19 @@ import com.tinkoff.android_homework.domain.main.entities.Total
 import javax.inject.Inject
 
 
-
+/**
+ * Преобразователь из общей суммы финансовых операций data-слоя (storage) в общую сумму
+ * финансовых операций domain-слоя.
+ */
 class TotalDbModelMapper @Inject constructor() : (TotalDbModel) -> Total {
 
+    /**
+     *  Преобразователь из общей суммы финансовых операций data-слоя (storage) в общую сумму
+     *  финансовых операций domain-слоя.
+     *
+     *  @param totalDbModel Общая сумма финансовых операций data-слоя.
+     *  @return Общая сумма финансовых операций domain-слоя.
+     */
     override fun invoke(totalDbModel: TotalDbModel): Total {
         return Total(amount = totalDbModel.total)
     }
