e117197b7a89a2bae969e9f98519abc1e3ae45d0
kerikir <vlasov-kerik@mail.ru>
2025-12-22 15:23:30 +0300

    added the NetworkOperationType to StorageOperationType mapper

--- app/src/main/java/com/tinkoff/android_homework/data/network/mappers/models/NetworkOperationTypeMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/mappers/models/NetworkOperationTypeMapper.kt
@@ -1,25 +1,25 @@
 package com.tinkoff.android_homework.data.network.mappers.models
 
 import com.tinkoff.android_homework.data.network.models.NetworkOperationType
-import com.tinkoff.android_homework.domain.main.models.OperationType
+import com.tinkoff.android_homework.data.storage.models.StorageOperationType
 import javax.inject.Inject
 
 
 /**
- * Преобразователь из типа операции data-слоя (network) в тип операций domain-слоя.
+ * Преобразователь из типа операции data-слоя (network) в тип операций data-слоя (storage).
  */
-class NetworkOperationTypeMapper @Inject constructor() : (NetworkOperationType) -> OperationType {
+class NetworkOperationTypeMapper @Inject constructor() : (NetworkOperationType) -> StorageOperationType {
 
     /**
-     *  Преобразователь из типа операции data-слоя (network) в тип операций domain-слоя.
+     *  Преобразователь из типа операции data-слоя (network) в тип операций data-слоя (storage).
      *
-     *  @param networkOperationType Тип финансовой операции data-слоя.
-     *  @return Тип финансовой операции domain-слоя.
+     *  @param networkOperationType Тип финансовой операции data-слоя (network).
+     *  @return Тип финансовой операции data-слоя (storage).
      */
-    override fun invoke(networkOperationType: NetworkOperationType): OperationType {
+    override fun invoke(networkOperationType: NetworkOperationType): StorageOperationType {
         return when (networkOperationType) {
-            NetworkOperationType.INCOME -> OperationType.INCOME
-            NetworkOperationType.OUTCOME -> OperationType.OUTCOME
+            NetworkOperationType.INCOME -> StorageOperationType.INCOME
+            NetworkOperationType.OUTCOME -> StorageOperationType.OUTCOME
         }
     }
 }
