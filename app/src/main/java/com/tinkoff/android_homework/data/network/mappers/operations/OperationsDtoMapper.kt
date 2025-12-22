876fe91644008ef624779f504180e6cce3744351
kerikir <vlasov-kerik@mail.ru>
2025-12-22 15:48:39 +0300

    added the operation type mapper in the OperationsDtoMapper constructor

--- app/src/main/java/com/tinkoff/android_homework/data/network/mappers/operations/OperationsDtoMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/mappers/operations/OperationsDtoMapper.kt
@@ -1,19 +1,22 @@
 package com.tinkoff.android_homework.data.network.mappers.operations
 
 import com.tinkoff.android_homework.data.network.entities.operations.OperationsDTO
+import com.tinkoff.android_homework.data.network.mappers.models.NetworkOperationTypeMapper
 import com.tinkoff.android_homework.data.storage.entities.OperationDbModel
 import javax.inject.Inject
 
 /**
  * @author d.shtaynmets
  */
-class OperationsDtoMapper @Inject constructor() : (OperationsDTO) -> List<OperationDbModel> {
+class OperationsDtoMapper @Inject constructor(
+    private val operationTypeMapper: NetworkOperationTypeMapper
+) : (OperationsDTO) -> List<OperationDbModel> {
 
     override fun invoke(operationsDTO: OperationsDTO): List<OperationDbModel> {
         return operationsDTO.operation.mapIndexed { index, operation ->
             OperationDbModel(
                 id = operation.id.toLong(),
-                type = operation.type,
+                type = operationTypeMapper(operation.type),
                 name = operation.name,
                 amount = operation.amount
             )
