2de225144b9ce80f8d89b5ff916458a7351d281c
kerikir <vlasov-kerik@mail.ru>
2025-12-22 15:34:45 +0300

    added the comments in the DetailDtoMapper.kt

--- app/src/main/java/com/tinkoff/android_homework/data/network/mappers/detail/DetailDtoMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/mappers/detail/DetailDtoMapper.kt
@@ -6,13 +6,25 @@ import com.tinkoff.android_homework.data.storage.entities.DetailDbModel
 import java.util.UUID
 import javax.inject.Inject
 
+
 /**
- * @author d.shtaynmets
+ * Преобразователь детального описания финансовой операции из типа data-слоя (network)
+ * в тип data-слоя (storage).
+ *
+ * @param operationTypeMapper Преобразователь из типа операции data-слоя (network)
+ * в тип операций data-слоя (storage)
  */
 class DetailDtoMapper @Inject constructor(
     private val operationTypeMapper: NetworkOperationTypeMapper
 ) : (DetailDTO) -> DetailDbModel {
 
+    /**
+     *  Преобразователь детального описания финансовой операции из типа data-слоя (network)
+     *  в тип data-слоя (storage).
+     *
+     *  @param detail Детальное описание финансовой операции data-слоя (network).
+     *  @return Детальное описание финансовой операции data-слоя (storage).
+     */
     override fun invoke(detail: DetailDTO): DetailDbModel {
         return DetailDbModel(
             id = UUID.randomUUID().timestamp(),
