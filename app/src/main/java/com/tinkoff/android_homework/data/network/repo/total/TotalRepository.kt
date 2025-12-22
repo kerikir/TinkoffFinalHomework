c964f29de3bd263179390c21d55872332ace8a0d
kerikir <vlasov-kerik@mail.ru>
2025-12-22 14:58:18 +0300

    renamed the TotalDbToDomainMapper.kt to TotalDbModelMapper.kt file

--- app/src/main/java/com/tinkoff/android_homework/data/network/repo/total/TotalRepository.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/repo/total/TotalRepository.kt
@@ -4,7 +4,7 @@ import com.tinkoff.android_homework.data.network.mappers.total.TotalApiToDbMappe
 import com.tinkoff.android_homework.data.network.repo.utils.InternetChecker
 import com.tinkoff.android_homework.data.network.services.TotalService
 import com.tinkoff.android_homework.data.storage.dao.TotalDao
-import com.tinkoff.android_homework.data.storage.mappers.total.TotalDbToDomainMapper
+import com.tinkoff.android_homework.data.storage.mappers.total.TotalDbModelMapper
 import com.tinkoff.android_homework.domain.main.entities.Total
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -23,7 +23,7 @@ class TotalRepositoryImpl @Inject constructor(
     private val totalDao: TotalDao,
     private val totalService: TotalService,
     private val totalApiToDbMapper: TotalApiToDbMapper,
-    private val totalDbToDomainMapper: TotalDbToDomainMapper,
+    private val totalDbModelMapper: TotalDbModelMapper,
     private val internetChecker: InternetChecker,
 ) : TotalRepository {
 
@@ -32,6 +32,6 @@ class TotalRepositoryImpl @Inject constructor(
             val totalApi = totalService.getTotal()
             totalDao.insert(totalApiToDbMapper.invoke(totalApi))
         }
-        return totalDao.getAll().map { totalDbToDomainMapper.invoke(it) }
+        return totalDao.getAll().map { totalDbModelMapper.invoke(it) }
     }
 }
--- app/src/main/java/com/tinkoff/android_homework/data/storage/mappers/total/TotalDbModelMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/mappers/total/TotalDbModelMapper.kt
@@ -4,13 +4,11 @@ import com.tinkoff.android_homework.data.storage.entities.TotalDbModel
 import com.tinkoff.android_homework.domain.main.entities.Total
 import javax.inject.Inject
 
-/**
- * @author d.shtaynmets
- */
-class TotalDbToDomainMapper @Inject constructor() : (TotalDbModel) -> Total {
+
+
+class TotalDbModelMapper @Inject constructor() : (TotalDbModel) -> Total {
 
     override fun invoke(totalDbModel: TotalDbModel): Total {
         return Total(amount = totalDbModel.total)
     }
-
 }
