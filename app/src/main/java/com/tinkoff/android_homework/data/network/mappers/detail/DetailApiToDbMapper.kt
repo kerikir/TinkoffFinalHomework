8fa41bb820f1b0550c4d4736db90666485b12933
kerikir <vlasov-kerik@mail.ru>
2025-12-22 15:20:57 +0300

    added the operation type mapper in the DetailApiToDbMapper constructor

--- app/src/main/java/com/tinkoff/android_homework/data/network/mappers/detail/DetailApiToDbMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/mappers/detail/DetailApiToDbMapper.kt
@@ -1,6 +1,7 @@
 package com.tinkoff.android_homework.data.network.mappers.detail
 
 import com.tinkoff.android_homework.data.network.entities.details.DetailDTO
+import com.tinkoff.android_homework.data.network.mappers.models.NetworkOperationTypeMapper
 import com.tinkoff.android_homework.data.storage.entities.DetailDbModel
 import java.util.UUID
 import javax.inject.Inject
@@ -8,15 +9,16 @@ import javax.inject.Inject
 /**
  * @author d.shtaynmets
  */
-class DetailApiToDbMapper @Inject constructor() : (DetailDTO) -> DetailDbModel {
+class DetailApiToDbMapper @Inject constructor(
+    private val operationTypeMapper: NetworkOperationTypeMapper
+) : (DetailDTO) -> DetailDbModel {
 
     override fun invoke(detail: DetailDTO): DetailDbModel {
         return DetailDbModel(
             id = UUID.randomUUID().timestamp(),
-            type = detail.type,
+            type = operationTypeMapper(detail.type),
             comment = detail.comment,
             positions = detail.positions
         )
     }
-
 }
