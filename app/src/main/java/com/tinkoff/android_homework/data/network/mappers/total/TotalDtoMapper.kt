93c5316a007fa69387409f162ab4d12daa19ed8f
kerikir <vlasov-kerik@mail.ru>
2025-12-22 16:35:33 +0300

    added the comments in the TotalDtoMapper.kt

--- app/src/main/java/com/tinkoff/android_homework/data/network/mappers/total/TotalDtoMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/mappers/total/TotalDtoMapper.kt
@@ -4,10 +4,20 @@ import com.tinkoff.android_homework.data.network.entities.total.TotalDTO
 import com.tinkoff.android_homework.data.storage.entities.TotalDbModel
 import javax.inject.Inject
 
+
 /**
- * @author d.shtaynmets
+ * Преобразователь общей суммы финансовых операций из типа data-слоя (network)
+ * в тип data-слоя (storage).
  */
 class TotalDtoMapper @Inject constructor(): (TotalDTO) -> TotalDbModel {
+
+    /**
+     *  Преобразователь общей суммы финансовых операций из типа data-слоя (network)
+     *  в тип data-слоя (storage).
+     *
+     *  @param totalDTO Общая сумма финансовых операций data-слоя (network).
+     *  @return Общая сумма финансовых операций data-слоя (storage).
+     */
     override fun invoke(totalDTO: TotalDTO): TotalDbModel {
         return TotalDbModel(
             id = totalDTO.id.toLong(),
