72589a78fa47bb20fce0d779613140f446c4daca
kerikir <vlasov-kerik@mail.ru>
2025-12-23 14:40:52 +0300

    added the comments in the TotalDao.kt

--- app/src/main/java/com/tinkoff/android_homework/data/storage/dao/TotalDao.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/dao/TotalDao.kt
@@ -7,15 +7,30 @@ import androidx.room.Query
 import com.tinkoff.android_homework.data.storage.entities.TotalDbModel
 import kotlinx.coroutines.flow.Flow
 
+
 /**
- * @author d.shtaynmets
+ * Data Access Object (DAO) для работы с общей суммы финансовых операций в базе данных.
+ *
+ * Предоставляет методы для CRUD операций.
  */
 @Dao
 interface TotalDao {
 
+    /**
+     * Получение всех записей из БД об общей суммы финансовых операций.
+     *
+     * @return Поток общей суммы финансовых операций из data-слоя (storage)
+     */
     @Query("SELECT * FROM ${TotalDbModel.TOTAL_TABLE_NAME}")
     fun getAll(): Flow<TotalDbModel>
 
+    /**
+     * Вставка нескольких строк в таблицу базы данных для хранения общей суммы финансовых операций.
+     *
+     * При повторении данных в БД, общая сумма финансовой операции заменяется на новое.
+     *
+     * @param totalDbModel Общая сумма финансовых операций из data-слоя (storage)
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(totalDbModel: TotalDbModel)
 }
