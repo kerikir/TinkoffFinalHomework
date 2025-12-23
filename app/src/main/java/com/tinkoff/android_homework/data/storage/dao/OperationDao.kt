3a2f0045a2871da2df2a6094b18113a306925dc0
kerikir <vlasov-kerik@mail.ru>
2025-12-23 14:29:01 +0300

    added the comments in the OperationDao.kt

--- app/src/main/java/com/tinkoff/android_homework/data/storage/dao/OperationDao.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/dao/OperationDao.kt
@@ -6,15 +6,30 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.tinkoff.android_homework.data.storage.entities.OperationDbModel
 
+
 /**
- * @author d.shtaynmets
+ * Data Access Object (DAO) для работы с финансовыми операциями в базе данных.
+ *
+ * Предоставляет методы для CRUD операций.
  */
 @Dao
 interface OperationDao {
 
+    /**
+     * Получение всех записей из БД о финансовых операций.
+     *
+     * @return Список финансовых операций из data-слоя (storage)
+     */
     @Query("SELECT * FROM ${OperationDbModel.OPERATION_TABLE_NAME}")
     suspend fun getAll(): List<OperationDbModel>
 
+    /**
+     * Вставка нескольких строк в таблицу базы данных для хранения финансовых операций.
+     *
+     * При повторении данных в БД, детальной описание финансовой операции заменяется на новое.
+     *
+     * @param operations Финансовые операции из data-слоя (storage)
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(vararg operations: OperationDbModel)
 }
