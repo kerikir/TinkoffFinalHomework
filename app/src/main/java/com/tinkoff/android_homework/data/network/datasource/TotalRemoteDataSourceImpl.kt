8724ac84cd4f7c3d6eb30273011ce70ed645e937
kerikir <vlasov-kerik@mail.ru>
2025-12-24 22:09:08 +0300

    added the implemented remote data source for the total

---
+++ app/src/main/java/com/tinkoff/android_homework/data/network/datasource/TotalRemoteDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.tinkoff.android_homework.data.network.datasource
+
+import com.tinkoff.android_homework.data.network.entities.total.TotalDTO
+import com.tinkoff.android_homework.data.network.services.TotalDtoService
+import javax.inject.Inject
+
+
+/** Удаленный источник данных для получения общей суммы финансовых операций. */
+class TotalRemoteDataSourceImpl @Inject constructor(
+    private val totalDtoService: TotalDtoService
+) : TotalRemoteDataSource {
+
+    /**
+     * Получение суммы всех финансовых операций
+     *
+     * @return Общая сумма финансовых операций из data-слоя (network)
+     */
+    override suspend fun getTotal(): TotalDTO {
+        return totalDtoService.getTotal()
+    }
+}
