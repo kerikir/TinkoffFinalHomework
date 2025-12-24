e34c07c80e12cbf67dc6793903c7c37e6f90cfc8
kerikir <vlasov-kerik@mail.ru>
2025-12-24 13:39:56 +0300

    added the comments in the DetailService.kt

--- app/src/main/java/com/tinkoff/android_homework/data/network/services/DetailService.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/services/DetailService.kt
@@ -4,11 +4,20 @@ import com.tinkoff.android_homework.data.network.entities.details.DetailDTO
 import retrofit2.http.GET
 import retrofit2.http.Path
 
+
 /**
- * @author d.shtaynmets
+ * Описание сетевых запросов для работы с детальным описанием финансовых операций.
  */
 interface DetailService {
 
+    /**
+     * Получение детального описания финансовой операции с сервера.
+     *
+     * GET-запрос.
+     *
+     * @param id Идентификатор финансовой операции.
+     * @return Детальное описание финансовой операции из data-слоя (network)
+     */
     @GET("android_for_begginers_back_end/main/detail/{id}.json")
     suspend fun getDetail(
         @Path("id") id: Int
