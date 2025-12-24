e9e2b621ca6417326e2f874e76d647dd597a956e
kerikir <vlasov-kerik@mail.ru>
2025-12-24 22:53:30 +0300

    added the detail remote data source in the repository constructor

--- app/src/main/java/com/tinkoff/android_homework/data/repo/detail/DetailRepositoryImpl.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/repo/detail/DetailRepositoryImpl.kt
@@ -1,9 +1,9 @@
 package com.tinkoff.android_homework.data.repo.detail
 
+import com.tinkoff.android_homework.data.network.datasource.DetailRemoteDataSource
 import com.tinkoff.android_homework.data.storage.mappers.detail.DetailDbModelMapper
 import com.tinkoff.android_homework.data.network.mappers.detail.DetailDtoMapper
 import com.tinkoff.android_homework.data.repo.utils.InternetChecker
-import com.tinkoff.android_homework.data.network.services.DetailDtoService
 import com.tinkoff.android_homework.data.storage.dao.DetailDbModelDao
 import com.tinkoff.android_homework.domain.main.entities.Detail
 import com.tinkoff.android_homework.domain.main.repos.DetailRepository
@@ -11,7 +11,7 @@ import javax.inject.Inject
 
 
 class DetailRepositoryImpl @Inject constructor(
-    private val detailDtoService: DetailDtoService,
+    private val detailRemoteDataSource: DetailRemoteDataSource,
     private val detailDbModelDao: DetailDbModelDao,
     private val detailDtoMapper: DetailDtoMapper,
     private val detailDbModelMapper: DetailDbModelMapper,
@@ -21,8 +21,8 @@ class DetailRepositoryImpl @Inject constructor(
 
     override suspend fun getDetail(id: Int): Detail {
         if (internetChecker.isInternetAvailable()) {
-            val detailApi = detailDtoService.getDetail(id)
-            detailDbModelDao.insertAll(detailDtoMapper.invoke(detailApi))
+            val detailDto = detailRemoteDataSource.getDetail(id)
+            detailDbModelDao.insertAll(detailDtoMapper(detailDto))
         }
 
         return detailDbModelMapper.invoke(detailDbModelDao.getAll())
