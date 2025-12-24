0f48f240bba8ba4171ce915068495b850c72a481
kerikir <vlasov-kerik@mail.ru>
2025-12-24 13:42:37 +0300

    added the comments in the OperationsService.kt

--- app/src/main/java/com/tinkoff/android_homework/data/network/services/OperationsService.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/services/OperationsService.kt
@@ -3,12 +3,19 @@ package com.tinkoff.android_homework.data.network.services
 import com.tinkoff.android_homework.data.network.entities.operations.OperationsDTO
 import retrofit2.http.GET
 
+
 /**
- * @author d.shtaynmets
+ * Описание сетевых запросов для работы с списком финансовых операций.
  */
 interface OperationsService {
 
+    /**
+     * Получение списка финансовых операций с сервера.
+     *
+     * GET-запрос.
+     *
+     * @return Список финансовых операций. из data-слоя (network)
+     */
     @GET("android_for_begginers_back_end/main/operations.json")
     suspend fun getOperations(): OperationsDTO
-
 }
