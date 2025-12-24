0b7cdf8ee6f2b9dafdd0c0927bdcf1b7f4f24978
kerikir <vlasov-kerik@mail.ru>
2025-12-24 14:02:07 +0300

    added the comments in the TotalService.kt

--- app/src/main/java/com/tinkoff/android_homework/data/network/services/TotalService.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/services/TotalService.kt
@@ -3,11 +3,19 @@ package com.tinkoff.android_homework.data.network.services
 import com.tinkoff.android_homework.data.network.entities.total.TotalDTO
 import retrofit2.http.GET
 
+
 /**
- * @author d.shtaynmets
+ * Описание сетевых запросов для работы общей суммой финансовых операций.
  */
 interface TotalService {
 
+    /**
+     * Получение общей суммы финансовых операций с сервера.
+     *
+     * GET-запрос.
+     *
+     * @return Общая сумма финансовых операций из data-слоя (network)
+     */
     @GET("android_for_begginers_back_end/main/total.json")
     suspend fun getTotal(): TotalDTO
 }
