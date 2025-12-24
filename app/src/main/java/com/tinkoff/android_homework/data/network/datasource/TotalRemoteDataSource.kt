69087a96f9407b831ae1efb2aa9c6ad44b8b844f
kerikir <vlasov-kerik@mail.ru>
2025-12-24 21:18:26 +0300

    added the remote data source for the total

---
+++ app/src/main/java/com/tinkoff/android_homework/data/network/datasource/TotalRemoteDataSource.kt
@@ -0,0 +1,11 @@
+package com.tinkoff.android_homework.data.network.datasource
+
+import com.tinkoff.android_homework.data.network.entities.total.TotalDTO
+
+
+/** Описание удаленного источника данных для получения общей суммы финансовых операций. */
+interface TotalRemoteDataSource {
+
+    /** Получение суммы всех финансовых операций */
+    suspend fun subscribeTotal(): TotalDTO
+}
