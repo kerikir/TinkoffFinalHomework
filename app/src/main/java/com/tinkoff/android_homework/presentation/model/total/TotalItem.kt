b58abd8be8062bc2c23aefc8a6d1dfe758168be8
kerikir <vlasov-kerik@mail.ru>
2025-12-25 17:38:13 +0300

    added the comments in the TotalItem.kt

--- app/src/main/java/com/tinkoff/android_homework/presentation/model/total/TotalItem.kt
+++ app/src/main/java/com/tinkoff/android_homework/presentation/model/total/TotalItem.kt
@@ -1,11 +1,18 @@
 package com.tinkoff.android_homework.presentation.model.total
 
+
 /**
- * @author d.shtaynmets
+ * Общая сумма финансовых операций.
+ *
+ * Модель данных для presentation слоя.
  */
 data class TotalItem(
+    /** Сумма всех операций */
     val total: Int,
+    /** Сумма входящих переводов */
     val income: Int,
+    /** Сумма расходов */
     val outcome: Int,
+    /** Отношение поступлений на счет и расходов */
     val progress: Float
 )
