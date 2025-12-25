7ae7594dc87ef6ad854f3dbc1484d8fc4429fc1b
kerikir <vlasov-kerik@mail.ru>
2025-12-25 16:54:48 +0300

    added the detail data model in the presentation layer

--- app/src/main/java/com/tinkoff/android_homework/presentation/model/DetailItem.kt
+++ app/src/main/java/com/tinkoff/android_homework/presentation/model/DetailItem.kt
@@ -1,7 +1,18 @@
 package com.tinkoff.android_homework.presentation.model
 
+import com.tinkoff.android_homework.domain.main.models.OperationType
+
+
 /**
- * @author d.shtaynmets
+ * Детальное описание финансовой операции.
+ *
+ * Модель данных для presentation слоя.
  */
-class DetailItem {
-}
+data class DetailItem (
+    /** Тип финансовой операции */
+    val type: OperationType,
+    /** Комментарий к финансовой операции для отображения */
+    val comment: String,
+    /** Список позиций (что входит в сумму операции) */
+    val positions: List<String>,
+)
