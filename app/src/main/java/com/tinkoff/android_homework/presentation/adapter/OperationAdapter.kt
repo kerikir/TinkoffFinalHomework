4f81651274005b272671ef565cde406dcef2ff08
kerikir <vlasov-kerik@mail.ru>
2025-12-26 13:15:15 +0300

    added the comments in the OperationAdapter.kt

--- app/src/main/java/com/tinkoff/android_homework/presentation/adapter/OperationAdapter.kt
+++ app/src/main/java/com/tinkoff/android_homework/presentation/adapter/OperationAdapter.kt
@@ -17,17 +17,25 @@ import com.tinkoff.android_homework.presentation.model.operations.PresentationOp
  */
 class OperationAdapter : RecyclerView.Adapter<OperationAdapter.OperationViewHolder>() {
 
+    /** Список элементов для отображения. */
     var data: List<OperationItem> = emptyList()
         set(newValue) {
             field = newValue
+            // Сообщает об изменениях (полная перерисовка)
             notifyDataSetChanged()
         }
 
 
-    /** Создание нового объекта View Holder. */
+    /**
+     * Создание нового объекта View Holder.
+     *
+     * @param parent Родительский ViewGroup - RecyclerView.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OperationViewHolder {
+        // Создание View из XML
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.operation_recycler_item, parent, false)
+        // Создание экземпляра View Holder
         return OperationViewHolder(itemView)
     }
 
@@ -39,13 +47,17 @@ class OperationAdapter : RecyclerView.Adapter<OperationAdapter.OperationViewHold
     /** Связывание данных с View Holder. */
     override fun onBindViewHolder(holder: OperationViewHolder, position: Int) {
         data[position].let { operationItem ->
+            // Определение иконки финансовой операции
             val operationIcon = when (operationItem.presentationOperationType) {
                 PresentationOperationType.OUTCOME -> R.drawable.spending_icon
                 PresentationOperationType.INCOME -> R.drawable.income_icon
             }
+            // Установка иконки финансовой операции
             holder.operationIcon.setImageResource(operationIcon)
 
+            // Установка текстового описания финансовой операции
             holder.operationTitle.text = operationItem.operationTitle
+            // Установка суммы финансовой операции
             holder.operationSum.text = operationItem.operationSum.toString()
         }
     }
@@ -54,8 +66,14 @@ class OperationAdapter : RecyclerView.Adapter<OperationAdapter.OperationViewHold
 
     /** Хранение ссылок на представления каждого элемента списка */
     class OperationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        // TODO заменить findViewById на View Binding
+
+        /** Ссылка на иконку финансовой операции */
         val operationIcon: ImageView = itemView.findViewById(R.id.operation_icon)
+        /** Ссылка на текстовое описание финансовой операции */
         val operationTitle: TextView = itemView.findViewById(R.id.operation_title)
+        /** Ссылка на сумму финансовой операции */
         val operationSum: TextView = itemView.findViewById(R.id.operation_sum)
     }
 }
