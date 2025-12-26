46f7345bb39a7ee6643215cb0b6ff8f29f0e4d88
kerikir <vlasov-kerik@mail.ru>
2025-12-26 12:51:27 +0300

    added the comments in the OperationItem.kt

--- app/src/main/java/com/tinkoff/android_homework/presentation/model/operations/OperationItem.kt
+++ app/src/main/java/com/tinkoff/android_homework/presentation/model/operations/OperationItem.kt
@@ -5,6 +5,8 @@ package com.tinkoff.android_homework.presentation.model.operations
  * Финансовая операция.
  *
  * Модель данных для presentation слоя.
+ *
+ * Модель данных для отображения в Recycler View.
  */
 data class OperationItem(
     /** Тип финансовой операции */
