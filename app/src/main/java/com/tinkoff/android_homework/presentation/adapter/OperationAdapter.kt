06f3dd24604c4643bd555e62fe304e981fada733
kerikir <vlasov-kerik@mail.ru>
2025-12-26 12:48:46 +0300

    added the comments in the OperationAdapter.kt

--- app/src/main/java/com/tinkoff/android_homework/presentation/adapter/OperationAdapter.kt
+++ app/src/main/java/com/tinkoff/android_homework/presentation/adapter/OperationAdapter.kt
@@ -10,8 +10,10 @@ import com.tinkoff.android_homework.R
 import com.tinkoff.android_homework.presentation.model.operations.OperationItem
 import com.tinkoff.android_homework.presentation.model.operations.PresentationOperationType
 
+
 /**
- * @author d.a.korotkov
+ * Преобразование объекта данных в элемент интерфейса.
+ * Управляет данными и связывает их с View Holder.
  */
 class OperationAdapter : RecyclerView.Adapter<OperationAdapter.OperationViewHolder>() {
 
@@ -21,14 +23,20 @@ class OperationAdapter : RecyclerView.Adapter<OperationAdapter.OperationViewHold
             notifyDataSetChanged()
         }
 
+
+    /** Создание нового объекта View Holder. */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OperationViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.operation_recycler_item, parent, false)
         return OperationViewHolder(itemView)
     }
 
+
+    /** Получение общего количества элементов. */
     override fun getItemCount(): Int = data.size
 
+
+    /** Связывание данных с View Holder. */
     override fun onBindViewHolder(holder: OperationViewHolder, position: Int) {
         data[position].let { operationItem ->
             val operationIcon = when (operationItem.presentationOperationType) {
@@ -42,6 +50,9 @@ class OperationAdapter : RecyclerView.Adapter<OperationAdapter.OperationViewHold
         }
     }
 
+
+
+    /** Хранение ссылок на представления каждого элемента списка */
     class OperationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val operationIcon: ImageView = itemView.findViewById(R.id.operation_icon)
         val operationTitle: TextView = itemView.findViewById(R.id.operation_title)
