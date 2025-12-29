099a272634af463600b461926fd7716e8b9a1bb2
kerikir <vlasov-kerik@mail.ru>
2025-12-29 17:42:00 +0300

    added the comments in the DetailLocalDataSourceImpl.kt

--- app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/DetailLocalDataSourceImpl.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/DetailLocalDataSourceImpl.kt
@@ -6,14 +6,27 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
+/** Локальный источник данных для получения детального описания финансовой операции. */
 class DetailLocalDataSourceImpl @Inject constructor(
     private val detailDao: DetailDbModelDao
 ) : DetailLocalDataSource {
 
+    /**
+     * Подписка на изменение дополнительной информации о финансовой операции.
+     *
+     * @param id Идентификатор финансовой операции.
+     * @return Поток с информацией о финансовой операции.
+     */
     override fun subscribeDetail(id: Int): Flow<DetailDbModel> {
         return detailDao.getDetailFlow(id)
     }
 
+
+    /**
+     * Вставка детального описания финансовых операций в локальный источник данных.
+     *
+     * @param detailOperations Детальное описание финансовых операций из data-слоя (storage).
+     */
     override suspend fun insertDetail(vararg detailOperations: DetailDbModel) {
         detailDao.insertAll(*detailOperations)
     }
