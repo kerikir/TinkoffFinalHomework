083a05aaa38bdd7b3fbce9331b39cf2e2b2f7ddb
kerikir <vlasov-kerik@mail.ru>
2025-12-29 16:37:31 +0300

    changed the name and return type method in the local data source interface

--- app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/DetailLocalDataSource.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/DetailLocalDataSource.kt
@@ -1,11 +1,12 @@
 package com.tinkoff.android_homework.data.storage.datasource
 
 import com.tinkoff.android_homework.data.storage.entities.DetailDbModel
+import kotlinx.coroutines.flow.Flow
 
 
 /** Описание локального источника данных для получения детального описания финансовой операции. */
 interface DetailLocalDataSource {
 
     /** Получение информации о финансовой операции */
-    suspend fun getDetail(id: Int): DetailDbModel
+    suspend fun subscribeDetail(id: Int): Flow<DetailDbModel>
 }
