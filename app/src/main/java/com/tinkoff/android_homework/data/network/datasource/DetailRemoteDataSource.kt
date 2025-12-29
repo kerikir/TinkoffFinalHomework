2b664462d3babb4af96938cb3faaf09c8caaa941
kerikir <vlasov-kerik@mail.ru>
2025-12-29 20:17:00 +0300

    changed the methods description in the remote data source

--- app/src/main/java/com/tinkoff/android_homework/data/network/datasource/DetailRemoteDataSource.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/datasource/DetailRemoteDataSource.kt
@@ -6,6 +6,11 @@ import com.tinkoff.android_homework.data.network.entities.details.DetailDTO
 /** Описание удаленного источника данных для получения детального описания финансовой операции. */
 interface DetailRemoteDataSource {
 
-    /** Получение информации о финансовой операции */
+    /**
+     * Получение информации о финансовой операции.
+     *
+     * @param id Идентификатор финансовой операции.
+     * @return Детальное описание финансовой операции из data-слоя (network).
+     */
     suspend fun getDetail(id: Int): DetailDTO
 }
