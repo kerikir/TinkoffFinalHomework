11cc081b2d7ec5c8e94e6782aa7dd7acca32f5cf
kerikir <vlasov-kerik@mail.ru>
2025-12-29 19:29:36 +0300

    added the comments in the TotalLocalDataSourceImpl.kt

--- app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/TotalLocalDataSourceImpl.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/TotalLocalDataSourceImpl.kt
@@ -6,14 +6,26 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
+/** Локальный источник данных для получения общей суммы финансовых операций. */
 class TotalLocalDataSourceImpl @Inject constructor(
     private val totalDao: TotalDbModelDao
 ) : TotalLocalDataSource {
 
+    /**
+     * Подписка на изменение записи из локального источника данных об общей сумме финансовых операций.
+     *
+     * @return Поток общей суммы финансовых операций из data-слоя (storage)
+     */
     override fun subscribeTotal(): Flow<TotalDbModel> {
         return totalDao.getTotalFlow()
     }
 
+
+    /**
+     * Сохранение общей суммы финансовых операци в локальном источнике данных.
+     *
+     * @param totalDbModel Общая сумма финансовых операций из data-слоя (storage)
+     */
     override suspend fun insertTotal(totalDbModel: TotalDbModel) {
         totalDao.insert(totalDbModel)
     }
