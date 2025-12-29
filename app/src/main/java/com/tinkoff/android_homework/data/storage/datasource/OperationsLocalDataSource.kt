517857510c2b034c97c518120b892463a5396391
kerikir <vlasov-kerik@mail.ru>
2025-12-29 17:52:40 +0300

    added the method in the local data source interface

--- app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/OperationsLocalDataSource.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/OperationsLocalDataSource.kt
@@ -1,12 +1,28 @@
 package com.tinkoff.android_homework.data.storage.datasource
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import com.tinkoff.android_homework.data.storage.entities.OperationDbModel
+import com.tinkoff.android_homework.domain.main.entities.Operations
 import kotlinx.coroutines.flow.Flow
 
 
 /** Описание локального источника данных для получения списка финансовых операций. */
 interface OperationsLocalDataSource {
 
-    /** Получение всех финансовых операций */
-    suspend fun subscribeOperations(): Flow<List<OperationDbModel>>
+    /**
+     * Подписка на изменение списка всех финансовых операций.
+     *
+     * @return Поток со списком всех финансовых операций.
+     */
+    fun subscribeOperations(): Flow<Operations>
+
+
+    /**
+     * Вставка нескольких строк в таблицу базы данных для хранения финансовых операций.
+     *
+     * @param operations Финансовые операции из data-слоя (storage).
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOperations(vararg operations: OperationDbModel)
 }
