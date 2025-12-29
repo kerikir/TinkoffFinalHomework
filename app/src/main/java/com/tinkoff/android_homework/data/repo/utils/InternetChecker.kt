4835e9def1eff673eef9d081dd6799e3e69f74bb
kerikir <vlasov-kerik@mail.ru>
2025-12-29 19:59:06 +0300

    added the provide internet checker in the network module DI

--- app/src/main/java/com/tinkoff/android_homework/data/repo/utils/InternetChecker.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/repo/utils/InternetChecker.kt
@@ -3,17 +3,13 @@ package com.tinkoff.android_homework.data.repo.utils
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 
 /**
  * Класс для проверки доступности интернета.
  */
-class InternetChecker @Inject constructor(
-    // Контекст приложения для доступа к системным сервисам Android
-    @ApplicationContext private val context: Context
-) {
+class InternetChecker (private val context: Context) {
 
     /** Проверка на активное сетевое подключение к интернету. */
     fun isInternetAvailable(): Boolean {
--- app/src/main/java/com/tinkoff/android_homework/di/NetworkModule.kt
+++ app/src/main/java/com/tinkoff/android_homework/di/NetworkModule.kt
@@ -1,18 +1,22 @@
 package com.tinkoff.android_homework.di
 
+import android.content.Context
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.tinkoff.android_homework.data.network.services.DetailDtoService
 import com.tinkoff.android_homework.data.network.services.OperationsDtoService
 import com.tinkoff.android_homework.data.network.services.TotalDtoService
+import com.tinkoff.android_homework.data.repo.utils.InternetChecker
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 
 /**
@@ -26,6 +30,7 @@ object NetworkModule {
 
     /** Предоставляем зависимость объекта для инициализации Retrofit */
     @Provides
+    @Singleton
     fun provideRetrofit(): Retrofit {
         // Перехватчик запросов
         val interceptor = HttpLoggingInterceptor()
@@ -51,6 +56,7 @@ object NetworkModule {
      * для работы с общей суммой финансовых операций
      */
     @Provides
+    @Singleton
     fun provideTotalService(retrofit: Retrofit): TotalDtoService {
         return retrofit.create(TotalDtoService::class.java)
     }
@@ -61,6 +67,7 @@ object NetworkModule {
      * для работы с списком финансовых операций
      */
     @Provides
+    @Singleton
     fun provideOperationsService(retrofit: Retrofit): OperationsDtoService {
         return retrofit.create(OperationsDtoService::class.java)
     }
@@ -71,9 +78,21 @@ object NetworkModule {
      * для работы с детальным описанием финансовых операций
      */
     @Provides
+    @Singleton
     fun provideDetailService(retrofit: Retrofit): DetailDtoService {
         return retrofit.create(DetailDtoService::class.java)
     }
+
+
+    /**
+     * Предоставляем зависимость объекта для проверки доступности интернета
+     */
+    @Provides
+    @Singleton
+    fun provideInternetChecker(
+        // Контекст приложения для доступа к системным сервисам Android
+        @ApplicationContext context: Context
+    ): InternetChecker = InternetChecker(context)
 
 
     /** Адрес сервера */
