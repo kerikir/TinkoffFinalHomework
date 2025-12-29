21231c31405251e2771a1036cbf2c65c096e0324
kerikir <vlasov-kerik@mail.ru>
2025-12-29 18:10:28 +0300

    added the methods in the local data source interface

--- app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/TotalLocalDataSource.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/TotalLocalDataSource.kt
@@ -3,9 +3,22 @@ package com.tinkoff.android_homework.data.storage.datasource
 import com.tinkoff.android_homework.data.storage.entities.TotalDbModel
 import kotlinx.coroutines.flow.Flow
 
+
 /** Описание локального источника данных для получения общей суммы финансовых операций. */
 interface TotalLocalDataSource {
 
-    /** Подписка на изменение суммы всех финансовых операций */
-    suspend fun subscribeTotal(): Flow<List<TotalDbModel>>
+    /**
+     * Подписка на изменение записи из локального источника данных об общей сумме финансовых операций.
+     *
+     * @return Поток общей суммы финансовых операций из data-слоя (storage)
+     */
+    fun subscribeTotal(): Flow<TotalDbModel>
+
+
+    /**
+     * Сохранение общей суммы финансовых операци в локальном источнике данных.
+     *
+     * @param totalDbModel Общая сумма финансовых операций из data-слоя (storage)
+     */
+    suspend fun insertTotal(totalDbModel: TotalDbModel)
 }
