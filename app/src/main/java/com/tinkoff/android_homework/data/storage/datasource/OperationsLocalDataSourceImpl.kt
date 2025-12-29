f68c225af74fc9f9426f5dd50626f9a81ee0eeb1
kerikir <vlasov-kerik@mail.ru>
2025-12-29 17:59:48 +0300

    added the comments in the OperationsLocalDataSourceImpl.kt

--- app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/OperationsLocalDataSourceImpl.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/OperationsLocalDataSourceImpl.kt
@@ -6,14 +6,26 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
+/** Локальный источник данных для получения списка финансовых операций. */
 class OperationsLocalDataSourceImpl @Inject constructor(
     private val operationDao: OperationDbModelDao
 ) : OperationsLocalDataSource {
 
+    /**
+     * Подписка на изменение списка всех финансовых операций.
+     *
+     * @return Поток со списком всех финансовых операций.
+     */
     override fun subscribeOperations(): Flow<List<OperationDbModel>> {
         return operationDao.getAllFlow()
     }
 
+
+    /**
+     * Вставка нескольких строк в таблицу базы данных для хранения финансовых операций.
+     *
+     * @param operations Финансовые операции из data-слоя (storage).
+     */
     override suspend fun insertOperations(vararg operations: OperationDbModel) {
         operationDao.insertAll(*operations)
     }
