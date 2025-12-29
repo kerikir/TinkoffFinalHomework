ad8c47018936b114459aaf2160442a7b49b81164
kerikir <vlasov-kerik@mail.ru>
2025-12-29 17:45:02 +0300

    changed the name and return type method in the local data source interface

--- app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/OperationsLocalDataSource.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/storage/datasource/OperationsLocalDataSource.kt
@@ -1,10 +1,12 @@
 package com.tinkoff.android_homework.data.storage.datasource
 
 import com.tinkoff.android_homework.data.storage.entities.OperationDbModel
+import kotlinx.coroutines.flow.Flow
+
 
 /** Описание локального источника данных для получения списка финансовых операций. */
 interface OperationsLocalDataSource {
 
     /** Получение всех финансовых операций */
-    suspend fun getOperations(): List<OperationDbModel>
+    suspend fun subscribeOperations(): Flow<List<OperationDbModel>>
 }
