3799a659c4a050a4a8c9989ce3a3894caffaf669
kerikir <vlasov-kerik@mail.ru>
2025-12-30 17:55:40 +0300

    changed the implementation for the mapper DetailDTO type to type DetailDbModel

--- app/src/main/java/com/tinkoff/android_homework/data/network/mappers/detail/DetailDtoMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/data/network/mappers/detail/DetailDtoMapper.kt
@@ -2,6 +2,7 @@ package com.tinkoff.android_homework.data.network.mappers.detail
 
 import com.tinkoff.android_homework.data.network.entities.details.DetailDTO
 import com.tinkoff.android_homework.data.network.mappers.models.NetworkOperationTypeMapper
+import com.tinkoff.android_homework.data.network.models.NetworkOperationType
 import com.tinkoff.android_homework.data.storage.entities.DetailDbModel
 import java.util.UUID
 import javax.inject.Inject
@@ -16,20 +17,23 @@ import javax.inject.Inject
  */
 class DetailDtoMapper @Inject constructor(
     private val operationTypeMapper: NetworkOperationTypeMapper
-) : (DetailDTO) -> DetailDbModel {
+) : (DetailDTO, Int, NetworkOperationType) -> DetailDbModel {
 
     /**
      *  Преобразователь детального описания финансовой операции из типа data-слоя (network)
      *  в тип data-слоя (storage).
      *
      *  @param detail Детальное описание финансовой операции data-слоя (network).
+     *  @param id Идентификатор финансовой операции
+     *  @param type Тип финансовой операции
      *  @return Детальное описание финансовой операции data-слоя (storage).
      */
-    override fun invoke(detail: DetailDTO): DetailDbModel {
-        // TODO изменить получение идентификатора
+    override fun invoke(detail: DetailDTO, id: Int, type: NetworkOperationType): DetailDbModel {
+        
         return DetailDbModel(
-            id = UUID.randomUUID().timestamp(),
-            type = operationTypeMapper(detail.type),
+            id = id,
+            type = operationTypeMapper(type),
+            amount = detail.amount,
             comment = detail.comment,
             positions = detail.positions
         )
