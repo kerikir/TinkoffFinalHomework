ea9654bd0da769d91ca86309b150a14d56a62642
kerikir <vlasov-kerik@mail.ru>
2025-12-31 13:50:02 +0300

    deleted the unnecessary code in the MainActivity.kt

--- app/src/main/java/com/tinkoff/android_homework/presentation/main/MainActivity.kt
+++ app/src/main/java/com/tinkoff/android_homework/presentation/main/MainActivity.kt
@@ -19,26 +19,6 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    // Модель представлений - ленивая инициализация
-    private val viewModel by viewModels<MainViewModel>()
-
-    /** Ссылка на текстовое представление - общая сумма на счету */
-    private lateinit var totalSum: TextView
-    /** Ссылка на текстовое представление - сумма расходов */
-    private lateinit var outcome: TextView
-    /** Ссылка на текстовое представление - общая начислений */
-    private lateinit var income: TextView
-    /** Ссылка на индикатор прогресса - отношение доходов к расходам */
-    private lateinit var progressBar: LinearProgressIndicator
-
-
-    /** Ссылка на представление с переработкой - список финансовых операций */
-    private lateinit var operationsRecyclerView: RecyclerView
-    /** Адаптер для Recycler View */
-    private val operationAdapter = OperationAdapter()
-
-
-
     /**
      * Создание Activity.
      *
@@ -48,84 +28,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Получение ссылки на Recycler View
-        operationsRecyclerView = findViewById(R.id.operations_recycler)
 
-        // Получение ссылки на TextView
-        totalSum = findViewById(R.id.sum)
-        // Получение ссылки на TextView
-        outcome = findViewById(R.id.outcome)
-        // Получение ссылки на TextView
-        income = findViewById(R.id.income)
-        // Получение ссылки на LinearProgressIndicator
-        progressBar = findViewById(R.id.progress_bar)
-
-        // Установка Adapter и LayoutManager для RecyclerView
-        initOperationsRecycler()
-
-        // Запуск потока для отслеживания изменения в списке финансовых операций
-        subscribeToOperations()
-        // Запуск потока для отслеживания изменения в статистике общей суммы финансовых операций
-        subscribeToTotal()
-    }
-
-
-
-    /**
-     * Подписка на изменение общей суммы финансовой операции.
-     * Статистика общей суммы финансовых операций отображается в отдельном файле макете.
-     */
-    private fun subscribeToTotal() {
-        // Запуск корутины на время жизни Activity
-        lifecycleScope.launch {
-            // Работа корутины в зависимости от жизненного цикла UI
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-
-                // Подписка на изменение статистики общей суммы финансовых операций
-                viewModel.total.collect { totalItem ->
-                    // Установка суммы начислений
-                    income.text = totalItem?.income.toString()
-                    // Установка суммы расходов
-                    outcome.text = totalItem?.outcome.toString()
-                    // Установка общей суммы финансовых операций
-                    totalSum.text = totalItem?.total.toString()
-                    // Обновление прогресса - отношения расходов и доходов
-                    progressBar.progress = totalItem?.progress?.toInt() ?: 0
-                }
-            }
-        }
-    }
-
-
-
-    /**
-     * Подписка на изменение данных списка операций.
-     * Список операций отображается в RecyclerView.
-     */
-    private fun subscribeToOperations() {
-        // Запуск корутины на время жизни Activity
-        lifecycleScope.launch {
-            // Работа корутины в зависимости от жизненного цикла UI
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-
-                // Подписка на изменение списка операций
-                viewModel.operations.collect {
-                    // Обновление данных списка операций - Recycler View
-                    operationAdapter.data = it
-                }
-            }
-        }
-    }
-
-
-
-    /**
-     *  Инициализация Recycler View для работы
-     */
-    private fun initOperationsRecycler() {
-        // Установка вида расположения элементов RecyclerView - вертикальный список
-        operationsRecyclerView.layoutManager = LinearLayoutManager(baseContext)
-        // Установка адаптера для RecyclerView
-        operationsRecyclerView.adapter = operationAdapter
     }
 }
