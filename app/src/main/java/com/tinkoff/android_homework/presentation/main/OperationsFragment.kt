6d9e8a730273feae8d66d2e89f6a7ea06a9d7a1e
kerikir <vlasov-kerik@mail.ru>
2025-12-31 13:44:03 +0300

    added the Operations Fragment in the project

---
+++ app/src/main/java/com/tinkoff/android_homework/presentation/main/OperationsFragment.kt
@@ -0,0 +1,15 @@
+package com.tinkoff.android_homework.presentation.main
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.tinkoff.android_homework.R
+
+class OperationsFragment : Fragment(R.layout.fragment_operations) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        
+    }
+}
