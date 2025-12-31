d801efd68172eb054cebffbbffddd120451fe5a7
kerikir <vlasov-kerik@mail.ru>
2025-12-31 13:55:44 +0300

    deleted the unnecessary code in the MainActivity.kt

--- app/src/main/java/com/tinkoff/android_homework/presentation/main/MainActivity.kt
+++ app/src/main/java/com/tinkoff/android_homework/presentation/main/MainActivity.kt
@@ -1,19 +1,9 @@
 package com.tinkoff.android_homework.presentation.main
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.tinkoff.android_homework.R
-import com.tinkoff.android_homework.presentation.adapter.OperationAdapter
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
