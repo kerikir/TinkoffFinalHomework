3e68fe24641c2124a060c2bba0aba5c10a504d54
kerikir <vlasov-kerik@mail.ru>
2025-12-31 19:25:17 +0300

    added the comments in the DetailItemMapper.kt

--- app/src/main/java/com/tinkoff/android_homework/presentation/mappers/details/DetailItemMapper.kt
+++ app/src/main/java/com/tinkoff/android_homework/presentation/mappers/details/DetailItemMapper.kt
@@ -5,10 +5,20 @@ import com.tinkoff.android_homework.presentation.mappers.models.PresentationOper
 import com.tinkoff.android_homework.presentation.model.detail.DetailItem
 import javax.inject.Inject
 
+
+/**
+ * Преобразователь описания финансовой операции из типа domain-слоя в тип presentation-слоя.
+ */
 class DetailItemMapper @Inject constructor(
     private val presentationOperationTypeMapper: PresentationOperationTypeMapper
 ) : (Detail) -> DetailItem {
 
+    /**
+     *  Преобразователь описания финансовой операции из типа domain-слоя в тип presentation-слоя.
+     *
+     *  @param detail Описание финансовой операции domain-слоя.
+     *  @return Описание финансовой операции presentation-слоя.
+     */
     override operator fun invoke(detail: Detail): DetailItem {
         return DetailItem(
             type = presentationOperationTypeMapper(detail.type),
