11d1df689178d77f6f4658fdff2286684069f9ea
kerikir <vlasov-kerik@mail.ru>
2025-12-31 17:06:16 +0300

    added the factory ViewModel with support AssistedInject

--- app/src/main/java/com/tinkoff/android_homework/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/tinkoff/android_homework/presentation/detail/DetailViewModel.kt
@@ -1,24 +1,31 @@
 package com.tinkoff.android_homework.presentation.detail
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.tinkoff.android_homework.domain.main.usecases.SubscribeDetailUseCase
 import com.tinkoff.android_homework.presentation.model.detail.DetailItem
-import dagger.hilt.android.lifecycle.HiltViewModel
+import com.tinkoff.android_homework.presentation.model.operations.PresentationOperationType
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-/**
- * @author d.shtaynmets
- */
-@HiltViewModel
-class DetailViewModel @Inject constructor(
+
+
+class DetailViewModel @AssistedInject constructor(
     private val subscribeDetailUseCase: SubscribeDetailUseCase,
+    @Assisted private val operationId: Int,
+    @Assisted private val operationType: PresentationOperationType
 ) : ViewModel() {
+
+    /** Фабрика ViewModel */
+    @dagger.assisted.AssistedFactory
+    interface Factory {
+        fun create(
+            operationId: Int,
+            operationType: PresentationOperationType
+        ) : DetailViewModel
+    }
 
     private val _details: MutableStateFlow<DetailItem?> = MutableStateFlow(null)
     val details: StateFlow<DetailItem?> = _details.asStateFlow()
